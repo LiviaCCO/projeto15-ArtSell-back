79c57764dff42b7bcf9cd0bf640144e7db9e0c34
LiviaCCO <livia.ccapistrano@gmail.com>
2023-05-01 15:15:30 -0300

    ajuste no joi

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -9,6 +9,6 @@ export const registerSchema = joi.object({
 })
 
 export const loginSchema = joi.object({
-    email: joi.string().email().required(),
+    cpfCnpj: joi.number().required(),
     password: joi.string().min(3).required()
 })
