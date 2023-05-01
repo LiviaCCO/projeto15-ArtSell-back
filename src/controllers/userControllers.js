f168b283f8a7539b1daf480deab69e93381b711e
LiviaCCO <livia.ccapistrano@gmail.com>
2023-05-01 10:19:24 -0300

    cadastro, login e logout

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -3,15 +3,17 @@ import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
 
 const signUp = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, cpfCnpj, birth } = req.body;
   const passwordHash = bcrypt.hashSync(password, 10);
 
   try {
     const user = await db.collection('users').findOne({ email });
     if (user) return res.status(409).send("E-mail já cadastrado!")
     await db.collection('users').insertOne({
+      cpfCnpj,
       name,
       email,
+      birth,
       password: passwordHash,
     });
     res.sendStatus(201);
@@ -21,12 +23,12 @@ const signUp = async (req, res) => {
 }
 
 const signIn = async (req, res) => {
-  const { email, password } = req.body;
+  const { cpfCnpj, password } = req.body;
   const token = uuid();
   
   try {
-    const user = await db.collection('users').findOne({ email });
-    if (!user) return res.status(404).send("O e-mail informado não é cadastrado");
+    const user = await db.collection('users').findOne({ cpfCnpj });
+    if (!user) return res.status(404).send("Usuário não cadastrado");
     
     const checkPassword = bcrypt.compareSync(password, user.password);
     if (!checkPassword) return res.status(401).send("Senha inválida");
--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,7 +1,9 @@
 import joi from "joi"
 
 export const registerSchema = joi.object({
+    cpfCnpj: joi.number().required(),
     name: joi.string().required(),
+    birth: joi.string().required(),
     email: joi.string().email().required(),
     password: joi.string().min(3).required()
 })
