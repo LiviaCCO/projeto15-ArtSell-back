3d27042d77f981e7dcc21d1fe0ff96232dd1cc86
LiviaCCO <livia.ccapistrano@gmail.com>
2023-05-01 14:13:01 -0300

    ajuste no joi

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -3,7 +3,7 @@ import joi from "joi"
 export const registerSchema = joi.object({
     cpfCnpj: joi.number().required(),
     name: joi.string().required(),
-    birth: joi.string().required(),
+    birth: joi.required(),
     email: joi.string().email().required(),
     password: joi.string().min(3).required()
 })
